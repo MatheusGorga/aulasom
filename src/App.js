2e6193671c480af7178da642499fcfa9f09cd67d
Matheus <matheus.gorga@b2breservas.com.br>
2022-04-04 18:13:05 -0300

    state com arrays e objetos

--- src/App.js
+++ src/App.js
@@ -1,67 +1,41 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
 
 class App extends Component {
   state = {
-    name: "Matheus Gorga",
-    counter: 0,
-  };
-
-  handleClick = () => {
-    this.setState({ name: "Dev Jr" });
-  };
-
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
+    posts: [
+      {
+        id: 1,
+        title: "Titulo 1",
+        description: "descrição 1",
+      },
+      {
+        id: 2,
+        title: "Titulo 2",
+        description: "descrição 2",
+      },
+      {
+        id: 3,
+        title: "Titulo 3",
+        description: "descrição 3",
+      },
+    ],
   };
 
   render() {
-    const { name, counter } = this.state;
+    const { posts } = this.state;
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handleClick}>
-            {name} - {counter}
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Link
-          </a>
-        </header>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.description}</p>
+          </div>
+        ))}
       </div>
     );
   }
 }
 
-/* function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
- */
 export default App;
